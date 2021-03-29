47ae5cf43d7c4aa802fa1387577db162d697faba
Davide Andreana <davin874@gmail.com>
2021-03-29 13:30:03 +0200

    ci ho provato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { LoginComponent } from './components/login/login.component';
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'heroes', component: HeroesComponent },
+  // { path: 'detail/:id', component: HeroDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
